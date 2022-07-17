4f8d3185e02c10a966064ef4c94a7703fd3a4537
FrolovIlia <85966869+FrolovIlia@users.noreply.github.com>
2022-07-17 18:23:37 +0300

    Update script for finding coordinates to tuple format.

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-
-$('.game_field').mousemove(function(e){
-    var target = this.getBoundingClientRect();
-    var x = e.clientX - target.left;
-    var y = e.clientY - target.top;
-    $('#coord-live').html(x + ', ' + y);
-
-});
-
 $('.game_field').click(function(e){
-    var target = this.getBoundingClientRect();
-    var x = e.clientX - target.left;
-    var y = e.clientY - target.top;
-    $('#coord-click').html(x + ', ' + y);
+    const target = this.getBoundingClientRect();
+    const y = e.clientX - target.left;
+    const x = e.clientY - target.top;
+    const size_sect = $('.game_field').width() / 10;
 
-    alert(x + ', ' + y +  + $('.game_field').width());
+    let coordinate_x = Math.floor(x/size_sect);
+    let coordinate_y = Math.floor(y/size_sect);
+
+    alert(coordinate_x + ", " + coordinate_y);
 });
-
-
-//TODO Нужно перевести координаты в номер ячейки.
-// Для этого, делим размер поля на 10, получаем размер одной ячейки. А потом, делим нацело координату по х,
-//     и соответствующую координату по y.
