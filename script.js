7d0466cb52f3f30e545850410dcdb6de064f04fe
FrolovIlia <85966869+FrolovIlia@users.noreply.github.com>
2022-08-03 21:28:05 +0300

    Add JSON inside script.js.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let data = {
+    "shipTypes": {
+        "carrier": { "size": 5, "count": 1 },
+        "battleship": { "size": 4, "count": 1 },
+        "cruiser": { "size": 3, "count": 1 },
+        "submarine": { "size": 3, "count": 1 },
+        "destroyer": { "size": 2, "count": 1 }
+    },
+    "layout": [
+        { "ship": "carrier", "positions": [[2,9], [3,9], [4,9], [5,9], [6,9]] },
+        { "ship": "battleship", "positions": [[5,2], [5,3], [5,4], [5,5]] },
+        { "ship": "cruiser", "positions": [[8,1], [8,2], [8,3]] },
+        { "ship": "submarine", "positions": [[3,0], [3,1], [3,2]] },
+        { "ship": "destroyer", "positions": [[0,0], [1,0]] }
+    ]
+}
+
+
 $('.game_field').click(function(e){
     const target = this.getBoundingClientRect();
     const y = e.clientX - target.left;
@@ -8,4 +26,6 @@ $('.game_field').click(function(e){
     let coordinate_y = Math.floor(y/size_sect);
 
     alert(coordinate_x + ", " + coordinate_y);
+    console.log(data)
 });
+
